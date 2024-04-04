9c11336f81bb82baa9ef64f316fb6679260655f4
David Crespo <david.crespo@oxidecomputer.com>
2024-04-03 22:00:33 -0500

    it plots!

--- plot-app/src/App.tsx
+++ plot-app/src/App.tsx
@@ -1,36 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import data from '../../output.json'
+import { useEffect, useRef } from 'react'
+import rawData from '../../output.json'
+import * as Plot from '@observablehq/plot'
 
-function App() {
-  const [count, setCount] = useState(0)
+type Point = { date: Date; area: string; count: number }
 
-  return (
-    <>
-      <div>
-        <a href='https://vitejs.dev' target='_blank'>
-          <img src={viteLogo} className='logo' alt='Vite logo' />
-        </a>
-        <a href='https://react.dev' target='_blank'>
-          <img src={reactLogo} className='logo react' alt='React logo' />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className='card'>
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className='read-the-docs'>
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+const data = rawData
+  .map(({ date, ...rest }) => ({ date: new Date(date), ...rest }))
+
+const cutoff = new Date(2022, 0, 1)
+
+const breakdown = data.filter(({ area, date }) => area !== 'Total' && date > cutoff)
+
+function Todos({ data }: { data: Point[] }) {
+  const containerRef = useRef(null)
+  useEffect(() => {
+    if (data === undefined || !containerRef.current) return
+    const plot = Plot.plot({
+      style: 'overflow: visible;',
+      y: { grid: true },
+      title: 'Open to-dos over time (breakdown)',
+      marks: [
+        Plot.lineY(data, { x: 'date', y: 'count', stroke: 'area' }),
+        Plot.text(
+          data,
+          Plot.selectLast({
+            x: 'date',
+            y: 'count',
+            z: 'area',
+            text: 'area',
+            textAnchor: 'start',
+            dx: 3,
+          }),
+        ),
+      ],
+    })
+    containerRef.current.append(plot)
+    return () => plot.remove()
+  }, [data])
+  return <div ref={containerRef}></div>
 }
 
-export default App
+export default function App() {
+  return <Todos data={breakdown} />
+}
