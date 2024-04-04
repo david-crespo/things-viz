ea7e9a06641c5aa42e24c3ef879ecc14fa7c1e99
David Crespo <david.crespo@oxidecomputer.com>
2024-04-03 22:40:45 -0500

    render all three charts

--- plot-app/src/App.tsx
+++ plot-app/src/App.tsx
@@ -2,44 +2,55 @@ import { useEffect, useRef } from 'react'
 import rawData from '../../output.json'
 import * as Plot from '@observablehq/plot'
 
-type Point = { date: Date; area: string; count: number }
+const getPlot = (label: string, data: typeof allData) =>
+  Plot.plot({
+    style: 'overflow: visible;',
+    y: { grid: true },
+    title: `Open to-dos (${label})`,
+    marks: [
+      Plot.lineY(data, { x: 'date', y: 'count', stroke: 'area' }),
+      Plot.text(
+        data,
+        Plot.selectLast({
+          x: 'date',
+          y: 'count',
+          z: 'area',
+          text: 'area',
+          textAnchor: 'start',
+          dx: 3,
+        }),
+      ),
+    ],
+  })
 
-const data = rawData
+const allData = rawData
   .map(({ date, ...rest }) => ({ date: new Date(date), ...rest }))
 
 const cutoff = new Date(2022, 0, 1)
 
-const breakdown = data.filter(({ area, date }) => area !== 'Total' && date > cutoff)
+const breakdownPlot = getPlot(
+  'breakdown',
+  allData.filter(({ area, date }) => area !== 'Total' && date > cutoff),
+)
+const totalPlot = getPlot('total', allData.filter(({ area }) => area === 'Total'))
+const oxidePlot = getPlot('Oxide', allData.filter(({ area }) => area === 'Oxide'))
 
-function Todos({ data }: { data: Point[] }) {
-  const containerRef = useRef(null)
+function RenderPlot({ plot }: { plot: ReturnType<typeof Plot.plot> }) {
+  const containerRef = useRef<HTMLDivElement>(null)
   useEffect(() => {
-    if (data === undefined || !containerRef.current) return
-    const plot = Plot.plot({
-      style: 'overflow: visible;',
-      y: { grid: true },
-      title: 'Open to-dos over time (breakdown)',
-      marks: [
-        Plot.lineY(data, { x: 'date', y: 'count', stroke: 'area' }),
-        Plot.text(
-          data,
-          Plot.selectLast({
-            x: 'date',
-            y: 'count',
-            z: 'area',
-            text: 'area',
-            textAnchor: 'start',
-            dx: 3,
-          }),
-        ),
-      ],
-    })
+    if (!containerRef.current) return
     containerRef.current.append(plot)
     return () => plot.remove()
-  }, [data])
+  }, [getPlot])
   return <div ref={containerRef}></div>
 }
 
 export default function App() {
-  return <Todos data={breakdown} />
+  return (
+    <>
+      <RenderPlot plot={breakdownPlot} />
+      <RenderPlot plot={totalPlot} />
+      <RenderPlot plot={oxidePlot} />
+    </>
+  )
 }
